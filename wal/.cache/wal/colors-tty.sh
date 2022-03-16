9d0f750af539703ed8dc5e1c22bd31884eb55439
Darylle Villanueva <ekflfdv29@gmail.com>
2022-03-16 23:49:04 +0800

    wal: update color schemes, add color palette

---
+++ wal/.cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0101010
+                 \e]P15F4C33
+                 \e]P2645A52
+                 \e]P3945538
+                 \e]P49D6330
+                 \e]P5E26024
+                 \e]P6D88634
+                 \e]P7e6ccb1
+                 \e]P8a18e7b
+                 \e]P95F4C33
+                 \e]PA645A52
+                 \e]PB945538
+                 \e]PC9D6330
+                 \e]PDE26024
+                 \e]PED88634
+                 \e]PFe6ccb1
+                 \ec'
