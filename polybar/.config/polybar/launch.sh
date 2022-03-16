f885ba0ba938bb135a66a0c36afae275f185710f
Darylle Villanueva <ekflfdv29@gmail.com>
2022-03-16 23:17:56 +0800

    polybar, i3: update config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 name=main
-bot=bottom
 
 pkill polybar
 
@@ -9,8 +8,6 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
 	MONITOR=eDP1 polybar --reload ${name}&
-	MONITOR=eDP1 polybar --reload ${bot}&
 else
 	polybar --reload ${name}&
-	polybar --reload ${bot}&
 fi
