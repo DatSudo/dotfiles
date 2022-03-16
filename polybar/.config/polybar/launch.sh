b882c2c9b1d667f52d95b79cc03e8800b4d1d7e3
Darylle Villanueva <ekflfdv29@gmail.com>
2022-03-16 10:31:31 +0800

    polybar: fixed launch command

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,14 +4,13 @@ name=main
 bot=bottom
 
 pkill polybar
-# killall -q polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
-	sleep 1 && MONITOR=eDP1 polybar --reload ${name}&
-	sleep 1 && MONITOR=eDP1 polybar --reload ${bot}&
+	MONITOR=eDP1 polybar --reload ${name}&
+	MONITOR=eDP1 polybar --reload ${bot}&
 else
-	sleep 1 && polybar --reload ${name}&
-	sleep 1 && polybar --reload ${bot}&
+	polybar --reload ${name}&
+	polybar --reload ${bot}&
 fi
